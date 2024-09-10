baf442197521e64276a860df6dd8a92295047a7d
Daniel Alejandro Miranda Villalta <daniel.alejandro.miranda.villalta@gmail.com>
2024-09-10 19:35:43 -0400

    Solicitud/Respuesta HTTP en Kotlin

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -3,32 +3,57 @@ package com.example.littlelemon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.lifecycleScope
 import com.example.littlelemon.ui.theme.LittleLemonTheme
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    private val responseLiveData = MutableLiveData<String>()
+    private val httpClient = HttpClient(Android)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LittleLemonTheme {
-                val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = Home.route) {
-                    composable(Home.route) {
-                        HomeScreen(navController)
-                    }
-                    composable(
-                        DishDetails.route + "/{${DishDetails.argDishId}}",
-                        arguments = listOf(navArgument(DishDetails.argDishId) { type = NavType.IntType })
-                    ) {
-                        val id = requireNotNull(it.arguments?.getInt(DishDetails.argDishId)) { "Dish id is null" }
-                        DishDetails(id)
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    val responseState = responseLiveData.observeAsState("").value
+                    Column {
+                        Button(
+                            onClick = {
+                                lifecycleScope.launch {
+                                    val response = fetchContent()
+                                    runOnUiThread {
+                                        responseLiveData.value = response
+                                    }
+                                }
+                            }
+                        ) {
+                            Text(text = "Download")
+                        }
+                        Text(text = responseState.toString())
                     }
                 }
             }
         }
+    }
+
+    private suspend fun fetchContent(): String {
+        return httpClient
+            .get("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/littleLemonMenu.json")
+            .bodyAsText()
     }
 }
